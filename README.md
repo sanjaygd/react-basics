1f5171c5f6a24eb693c0380b6a181d78046b4956
Git Lab <git@lab.com>
2023-02-27 15:00:53 +0530

    states

--- README.md
+++ README.md
@@ -8,3 +8,6 @@
 
 #### Chapter-4 : [Class Component](https://github.com/sanjaygd/react-basics/tree/ef56c586386447b1fe387e641c7c54a194122976)
 
+#### Chapter-5 : [Class Component](https://github.com/sanjaygd/react-basics/tree/c4b9add532d1804303aa28c7a94f0e1a98f867d9)
+
+
--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,17 +1,14 @@
 import './App.css';
 import {Meet} from './components/Greet';
 import Welcome from './components/Welcome';
+import Message from './components/Message';
 
 // import Meet from './components/Greet';
 
 function App() {
   return (
     <div className="App">
-      <Meet name='Bruce' heroName='Batman' />
-      <Meet name='Cark' heroName='Super Man'>
-        <p>This is children Component</p>
-      </Meet>      
-      <Welcome name="Diana" heroName="Wonder Women"/>
+      <Message/>
     </div>
   );
 }
